67bb67984aa962fdbf7a71a298c5a793e5569948
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2021-07-28 13:04:22 -0500

    Initial Commit

---
+++ GraphQLCountry/GraphQLCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLCountryApp.swift
+//  GraphQLCountry
+//
+//  Created by Miguel Mexicano Herrera on 28/07/21.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
