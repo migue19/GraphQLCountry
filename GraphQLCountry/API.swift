d5d811764f3f030f938b89126cb03e3b90f787ca
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2021-07-29 09:34:25 -0500

    Se agrega Queries.graphql y API. swift con los modelos para la consulta

---
+++ GraphQLCountry/API.swift
@@ -0,0 +1,199 @@
+// @generated
+//  This file was automatically generated and should not be edited.
+
+import Apollo
+import Foundation
+
+public final class SpecificCountryQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query SpecificCountry {
+      country(code: "US") {
+        __typename
+        name
+        native
+        capital
+        currency
+        emoji
+        emojiU
+        states {
+          __typename
+          name
+        }
+      }
+    }
+    """
+
+  public let operationName: String = "SpecificCountry"
+
+  public init() {
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Query"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("country", arguments: ["code": "US"], type: .object(Country.selections)),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(country: Country? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Query", "country": country.flatMap { (value: Country) -> ResultMap in value.resultMap }])
+    }
+
+    public var country: Country? {
+      get {
+        return (resultMap["country"] as? ResultMap).flatMap { Country(unsafeResultMap: $0) }
+      }
+      set {
+        resultMap.updateValue(newValue?.resultMap, forKey: "country")
+      }
+    }
+
+    public struct Country: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Country"]
+
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("name", type: .nonNull(.scalar(String.self))),
+          GraphQLField("native", type: .nonNull(.scalar(String.self))),
+          GraphQLField("capital", type: .scalar(String.self)),
+          GraphQLField("currency", type: .scalar(String.self)),
+          GraphQLField("emoji", type: .nonNull(.scalar(String.self))),
+          GraphQLField("emojiU", type: .nonNull(.scalar(String.self))),
+          GraphQLField("states", type: .nonNull(.list(.nonNull(.object(State.selections))))),
+        ]
+      }
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(name: String, native: String, capital: String? = nil, currency: String? = nil, emoji: String, emojiU: String, states: [State]) {
+        self.init(unsafeResultMap: ["__typename": "Country", "name": name, "native": native, "capital": capital, "currency": currency, "emoji": emoji, "emojiU": emojiU, "states": states.map { (value: State) -> ResultMap in value.resultMap }])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var name: String {
+        get {
+          return resultMap["name"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "name")
+        }
+      }
+
+      public var native: String {
+        get {
+          return resultMap["native"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "native")
+        }
+      }
+
+      public var capital: String? {
+        get {
+          return resultMap["capital"] as? String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "capital")
+        }
+      }
+
+      public var currency: String? {
+        get {
+          return resultMap["currency"] as? String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "currency")
+        }
+      }
+
+      public var emoji: String {
+        get {
+          return resultMap["emoji"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "emoji")
+        }
+      }
+
+      public var emojiU: String {
+        get {
+          return resultMap["emojiU"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "emojiU")
+        }
+      }
+
+      public var states: [State] {
+        get {
+          return (resultMap["states"] as! [ResultMap]).map { (value: ResultMap) -> State in State(unsafeResultMap: value) }
+        }
+        set {
+          resultMap.updateValue(newValue.map { (value: State) -> ResultMap in value.resultMap }, forKey: "states")
+        }
+      }
+
+      public struct State: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["State"]
+
+        public static var selections: [GraphQLSelection] {
+          return [
+            GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+            GraphQLField("name", type: .nonNull(.scalar(String.self))),
+          ]
+        }
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(name: String) {
+          self.init(unsafeResultMap: ["__typename": "State", "name": name])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var name: String {
+          get {
+            return resultMap["name"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "name")
+          }
+        }
+      }
+    }
+  }
+}
